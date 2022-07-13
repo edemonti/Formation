21771700c2fc46f4f49880b340a329804409b999
Emmanuel de MONTI <edemonti@hotmail.fr>
2022-07-13 19:15:10 +0200

    [TodoMVC] Configuration base de données sur autre pc.

--- solution/TodoMVC/TodoMVC/TodoMVC/Migrations/202207080714311_Initial.Designer.cs
+++ solution/TodoMVC/TodoMVC/TodoMVC/Migrations/202207080714311_Initial.Designer.cs
@@ -6,7 +6,7 @@ namespace TodoMVC.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
+    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
     public sealed partial class Initial : IMigrationMetadata
     {
         private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
