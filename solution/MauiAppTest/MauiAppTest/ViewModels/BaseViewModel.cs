5d2718e4cb720d90a684ac26ac20b91f991221d7
Emmanuel de MONTI <edemonti@hotmail.fr>
2022-08-07 22:34:02 +0200

    [TodoBlazor] Création d'une classe spécifique pour la code de fonctionnement.

---
+++ solution/MauiAppTest/MauiAppTest/ViewModels/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MauiAppTest.ViewModels;
+
+public partial class BaseViewModel : ObservableObject
+{
+    
+    #region Properties
+
+    /// <summary>
+    /// Flag indiquant si l’appareil est occupé.
+    /// </summary>
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    private bool isBusy;
+
+    /// <summary>
+    /// Flag indiquant si la page est en cours de rafraîchissement.
+    /// </summary>
+    [ObservableProperty]
+    private bool isRefreshing;
+
+    /// <summary>
+    /// Flag indiquant si l’appareil n’est pas occupé.
+    /// </summary>
+    public bool IsNotBusy => !IsBusy;
+
+    /// <summary>
+    /// Titre de la page.
+    /// </summary>
+    [ObservableProperty]
+    private string title;
+
+    #endregion
+
+}
